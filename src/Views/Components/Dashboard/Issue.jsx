512feed29abe5bd195dba1b196ae4401bc227530
jesse h <jesselevihall@gmail.com>
2022-05-17 17:49:27 -0600

    dashboard display bugs

--- src/Views/Components/Dashboard/Issue.jsx
+++ src/Views/Components/Dashboard/Issue.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Priority from "../../../Controllers/priorityController";
+import { Card, Row, Col, CardTitle, CardText, Button } from "reactstrap";
 
 export default (props) => {
   const { level, color } = Priority(props.priority);
   return (
     <div className="container-sm">
       <div style={{ color: color }}>
-        <h2>Total: {level} </h2>
-        <p>{props.count}</p>
+        <Card body color="info" inverse>
+          <CardTitle tag="h5">Special Title Treatment</CardTitle>
+          <CardText>
+            <p>Total: {props.count}</p>
+            <p>Priority: {level}</p>
+          </CardText>
+          <Button>Button</Button>
+        </Card>
       </div>
     </div>
   );
--- src/Views/Pages/Dashboard/Dashboard.jsx
+++ src/Views/Pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Row, Col } from "reactstrap";
 import Issue from "../../Components/Dashboard/Issue";
 
-export default (props) => {
+export default () => {
   return (
     <div className="container-sm">
-      <Issue priority="1" count="10" />
+      <Row>
+        <Col className="mt-5" md="6">
+          <Issue priority="1" count="10" />
+        </Col>
+        <Col className="mt-5" md="6">
+          <Issue priority="2" count="10" />
+        </Col>
+        <Col className="mt-5" md="6">
+          <Issue priority="3" count="10" />
+        </Col>
+      </Row>
     </div>
   );
 };
