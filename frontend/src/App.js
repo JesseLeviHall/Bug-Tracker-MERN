c5165d4f0e11220fb18823600114176c2900179c
jesse h <jesselevihall@gmail.com>
2022-06-03 15:13:30 -0600

    use params

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,7 +28,7 @@ function App() {
             <Route path="/create">
               <BugForm />
             </Route>
-            <Route path="/viewbugs/bugId">
+            <Route path="/viewbugs/:bugId">
               <EditBugForm />
             </Route>
           </Switch>
