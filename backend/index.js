f60fe063236ba8ab8470a3db129bce89af3fcf29
jesse h <jesselevihall@gmail.com>
2022-06-09 18:23:15 -0600

    small update

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,12 @@ const app = express();
 
 //app.use("/auth", auth);
 
-app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "50000mb", extended: true }));
+app.use(express.urlencoded({ limit: "50000mb", extended: true }));
 app.use(cors());
 
 app.use("/viewbugs", viewBugs);
+
 //app.use(cookieParser());
 
 const PORT = process.env.PORT || 3500;
