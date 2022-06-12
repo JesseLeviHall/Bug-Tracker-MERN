dce1020ee0eca26b0e3282c66dec0a43702e778d
jesse h <jesselevihall@gmail.com>
2022-06-12 16:42:06 -0600

    update card appearance

--- frontend/src/Views/Components/Bug Card/bugCard.jsx
+++ frontend/src/Views/Components/Bug Card/bugCard.jsx
@@ -1,30 +1,54 @@
 import React from "react";
-import { Col, Card, CardBody, CardTitle, CardText, Button } from "reactstrap";
-import Priority from "../../../Controllers/priorityController";
+import {
+  Col,
+  ListGroupItem,
+  ListGroup,
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+  Button,
+} from "reactstrap";
 import moment from "moment";
 import { Link } from "react-router-dom";
 
 export default function bugCard(props) {
-  const { color } = Priority(props.priority);
-  const formatDate = moment().format("MM-DD-YYYY");
-
   return (
     <Col className="mt-5" md="4">
-      <Card className="mt-2" style={{ backgroundColor: color }}>
+      <Card className="mt-2">
         <CardTitle className="mt-2 text-center" tag="h5">
           {props.bug.name}
         </CardTitle>
         <CardBody>
-          <CardText className="text-left ms-3">
-            Assigned: {props.bug.assigned} <br />
-            Status: {props.bug.completed === false ? "Resolved" : "Active"}
-            <br />
-            Priority: {props.bug.priority} <br />
-            Created: {moment(props.bug.time).format("MM-DD-YY")} <br />
-            Details: {props.bug.details} <br />
-            Steps: {props.bug.steps} <br />
-            Webpage: {props.bug.webpage} <br />
-          </CardText>
+          <ListGroup flush>
+            <ListGroupItem>
+              Assigned: <span className="ms-2">{props.bug.assigned}</span>
+            </ListGroupItem>
+            <ListGroupItem>
+              Status:
+              <span className="ms-2">
+                {props.bug.completed === false ? "Resolved" : "Active"}
+              </span>
+            </ListGroupItem>
+            <ListGroupItem>
+              Priority: <span className="ms-2">{props.bug.priority}</span>
+            </ListGroupItem>
+            <ListGroupItem>
+              Created:{" "}
+              <span className="ms-2">
+                {moment(props.bug.time).format("MM-DD-YY")}
+              </span>
+            </ListGroupItem>
+            <ListGroupItem>
+              Details: <span className="ms-2">{props.bug.details}</span>
+            </ListGroupItem>
+            <ListGroupItem>
+              Steps: <span className="ms-2">{props.bug.steps}</span>
+            </ListGroupItem>
+            <ListGroupItem>
+              Webpage: <span className="ms-2">{props.bug.webpage}</span>
+            </ListGroupItem>
+          </ListGroup>
           <Button outline color="info" className="ms-3 mt-3" size="sm">
             <Link to={`/editbug/${props.bug._id}`}>Edit Bug</Link>
           </Button>
