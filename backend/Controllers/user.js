8dbf81b1ac4ebef3366f9c8a6bb20f652efa1a26
jesse h <jesselevihall@gmail.com>
2022-06-17 08:31:45 -0600

    working on login route

--- backend/Controllers/user.js
+++ backend/Controllers/user.js
@@ -3,6 +3,11 @@ import jwt from "jsonwebtoken";
 
 import User from "../Models/userModel.js";
 const secret = "test";
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 export const signup = async (req, res) => {
   const { firstName, lastName, userName, password } = req.body;
@@ -23,17 +28,31 @@ export const signup = async (req, res) => {
     password: hashedPassword,
   });
 
-  const token = jwt.sign({ id: result._id }, secret, { expiresIn: "1h" });
-
   if (result) {
     res.status(201).json({
       _id: result._id,
       firstName: result.firstName,
       lastName: result.lastName,
-      token: token,
+      token: generateToken(result._id),
     });
   } else {
     res.status(400).json({ message: "Something went wrong" });
+    console.log(error);
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { userName, password } = req.body;
+  const user = await User.findOne({ userName });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      token: token,
+    });
+  } else {
+    res.status(400).json({ message: "password did no match data" });
     console.log(error);
   }
 };
--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/user.js";
+import { loginUser, signup } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post("/", signup);
+router.post("/login", loginUser);
 
 export default router;
