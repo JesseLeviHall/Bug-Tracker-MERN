f7cba92580670916affc98da997a5d17e5d15cf9
jesse h <jesselevihall@gmail.com>
2022-06-23 12:37:55 -0600

    My bugs filter functional

--- frontend/src/Views/Pages/MyBugs.jsx
+++ frontend/src/Views/Pages/MyBugs.jsx
@@ -3,7 +3,7 @@ import {
   fetchBugs,
   getBugsError,
   getBugsStatus,
-  selectBugsByUser,
+  selectAllBugs,
 } from "../../Controllers/Reducers/bugSlice";
 import { useSelector, useDispatch } from "react-redux";
 import Navigation from "../Components/Navagation/Navigation";
@@ -11,7 +11,8 @@ import MyBugCard from "../Components/MyBugCard";
 import { Row } from "reactstrap";
 
 export default function MyBugs() {
-  const bugs = useSelector(selectBugsByUser);
+  const user = JSON.parse(localStorage.getItem("user"));
+  const bugs = useSelector(selectAllBugs);
   const bugStatus = useSelector(getBugsStatus);
   const error = useSelector(getBugsError);
   const dispatch = useDispatch();
@@ -22,11 +23,13 @@ export default function MyBugs() {
     }
   }, [bugStatus, dispatch]);
 
+  const myBugs = bugs.filter((bug) => bug.assigned === user.userName);
+
   let content;
   if (bugStatus === "loading") {
     content = <p className="text-center mt-5"> Loading...</p>;
   } else if (bugStatus === "succeeded") {
-    content = bugs.map((bug) => <MyBugCard key={bug._id} bug={bug} />);
+    content = myBugs.map((bug) => <MyBugCard key={bug._id} bug={bug} />);
   } else if (bugStatus === "failed") {
     content = <p>{error}</p>;
   }
